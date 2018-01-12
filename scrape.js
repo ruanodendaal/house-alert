bf2434ec3a042013376e07e551941b9fe0f0db23
ruanodendaal <ruanodendaal@gmail.com>
2018-01-12 16:36:28 +0000

    fix arrow function issue

--- scrape.js
+++ scrape.js
@@ -86,4 +86,6 @@ app.get('/', function(req, res) {
   findHouses(ripleyView);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
